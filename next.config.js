a5397ebc8db96349890b42e456cc4a784e7c8180
Hieu Nguyen <hieunguyenel@gmail.com>
2021-04-02 15:39:33 +0700

    build stuff

--- next.config.js
+++ next.config.js
@@ -14,6 +14,7 @@ module.exports = withCommerceConfig({
     locales: ['en-US', 'es'],
     defaultLocale: 'en-US',
   },
+  target: 'serverless'
   rewrites() {
     return [
       (isBC || isShopify) && {
