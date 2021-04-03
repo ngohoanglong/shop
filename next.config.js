f0661b24ca8e7a693fc6f7f2a890460ea67ec717
Hieu Nguyen <hieunguyenel@gmail.com>
2021-04-03 07:51:03 +0700

    nextjs images

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@ module.exports = withCommerceConfig({
     locales: ['en-US', 'es'],
     defaultLocale: 'en-US',
   },
+  images: {
+    domains: ['example.com'],
+  },
   target: 'serverless',
   rewrites() {
     return [
